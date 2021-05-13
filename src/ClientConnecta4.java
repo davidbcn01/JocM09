d9a7462418dc8d4242e4d450a276617252675fc5
David Ferrero <dferre@elpuig.xeill.net>
2021-05-13 19:17:08 +0200

    Initial commit

---
+++ src/ClientConnecta4.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+
+public class ClientConnecta4 {
+    private int portDesti;
+    private int result;
+    private String Nom, ipSrv;
+    private int intents;
+    private InetAddress adrecaDesti;
+    private Tauler t;
+    private Jugada j;
+    public ClientConnecta4(){
+
+    }
+}
---
+++ src/ServidorConnecta4.java
@@ -0,0 +1,2 @@
+public class ServidorConnecta4 {
+}
